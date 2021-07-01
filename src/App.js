7bae529079d67f1a62d93fb677d81ae0a9ff3a8d
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-01 13:41:19 +0000

    change hovers etc other for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ export default function Example() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'border-2 border-gray-900 text-black' : 'text-black hover:border-2 border-gray-700 hover:text-white',
+                          item.current ? 'border-2 border-gray-900 text-black' : 'text-black hover:border-2 border-gray-700 hover:text-gray-900',
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -84,7 +84,7 @@ export default function Example() {
                   key={item.name}
                   href={item.href}
                   className={classNames(
-                    item.current ? 'border-2 border-gray-900 text-black' : 'text-black hover:border-2 border-gray-700 hover:text-white',
+                    item.current ? 'underline text-black' : 'text-black',
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
