4518689ad4354493a4da3ddfca9ce42dfd887204
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-01 13:16:19 +0000

    fix bugs

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function Example() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button className="bg-gray-800 p-1 rounded-full text-black>
+                <button className="bg-gray-800 p-1 rounded-full text-black">
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
