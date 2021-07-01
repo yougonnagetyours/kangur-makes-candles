cc6c9b27d30c015291e4f3d046475d9c1f103388
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-01 13:25:02 +0000

    change bg to white

--- src/App.js
+++ src/App.js
@@ -65,11 +65,11 @@ export default function Example() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button className="bg-gray-800 p-1 rounded-full text-black">
+                <button className="bg-white p-1 rounded-full text-black">
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-                <button className="bg-gray-800 p-1 rounded-full text-black focus:outline-none">
+                <button className="bg-white p-1 rounded-full text-black focus:outline-none">
                   <span className="sr-only">View notifications</span>
                   <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
