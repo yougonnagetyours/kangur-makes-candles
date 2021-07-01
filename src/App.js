001a132e504cc3d6a54ee93b3e6da5e37f465dd1
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-01 13:55:27 +0000

    change main function name

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function App() {
   return (
     <Disclosure as="nav" className="bg-white">
       {({ open }) => (
