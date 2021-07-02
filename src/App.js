ee424b6022ba2714dae9718fe6b24e713c91b956
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-02 10:55:03 +0000

    set main section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Nav from './components/Nav.js'
-
-//import main1Mobile from './pics/main1-mobile.jpg'
+import main1Mobile from './pics/main1-mobile.jpg'
 
 
 export default function App() {
@@ -10,12 +9,21 @@ export default function App() {
       <Nav />
       <main>
         {/* Here are components which are switched by react router */}
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="main1Container">
+            <img 
+              className="block lg:hidden h-8 w-auto" 
+              src={main1Mobile} 
+              alt="candles" 
+            />
+          </div>
+        </div>
         {/* <MainSite /> */}
+        {/* --> <Shop /> */}
         {/* <Products /> */}
-        {/* <Shop /> */}
-        {/* <Contact /> */}
+        {/* <Shop />     */}
+        {/* <Contact />  */}
       </main>
-      {/* <Products /> */}
       {/* <Footer /> */}
     </div>
   )
