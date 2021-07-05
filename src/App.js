a04741e87ec10a601a23916aef462e362c6e1bde
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-05 09:21:48 +0000

    reset 2ndrow children margin

--- src/App.js
+++ src/App.js
@@ -20,28 +20,28 @@ export default function App() {
           <p className="text-2xl sm:text-4xl mt-3 tracking-wider">Witaj w soyaCandles</p>
         </div>
         <div className="2ndrow flex flex-wrap justify-between box-border mx-6 ">
-          <div className="w-1/2 mr-1">
+          <div className="w-1/2">
             <img 
               className="block" 
               src={main1Mobile} 
               alt="candles" 
             />
           </div>
-          <div className="w-1/2 ml-1">
+          <div className="w-1/2">
             <img 
               className="block" 
               src={main1Mobile} 
               alt="candles" 
             />
           </div>
-          <div className="w-1/2 mr-1">
+          <div className="w-1/2">
             <img 
               className="block" 
               src={main1Mobile} 
               alt="candles" 
             />
           </div>
-          <div className="w-1/2 ml-1">
+          <div className="w-1/2">
             <img 
               className="block" 
               src={main1Mobile} 
