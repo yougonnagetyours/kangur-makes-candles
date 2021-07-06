2a96ded55416a31777105e2da2b81575463b3b9a
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-07-06 21:41:03 +0200

    create mainSite -please fix bugs - colosure in JSX

---
+++ src/components/MainSite.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+
+export default function MainSite() {
+  return (
+<div className="1strow box-border mb-10 lg:flex lg:mb-20">
+          <div className="lg:mr-3">
+            <img 
+              className="block" 
+              src={main1Mobile} 
+              alt="candles" 
+            />
+          </div>
+          <div className="mt-6">
+            <p className="text-2xl sm:text-4xl mx-6 tracking-wider">Witaj w soyaCandles</p>
+            <p className="text-sm sm:text-xl mx-6 tracking-wider">Poznaj produkty bamburcia</p>
+          </div>
+        </div>
+        <div className="2ndrow flex flex-wrap justify-around box-border mx-2.5 my-6 ">
+          <div className="border border-black w-2/5 p-5 mb-6">
+            <img 
+              className="block" 
+              src={candle1} 
+              alt="candles" 
+            />
+          </div>
+          <div className="border border-black w-2/5 p-5 mb-6">
+            <img 
+              className="block" 
+              src={candle2} 
+              alt="candles" 
+            />
+          </div>
+          <div className="border border-black w-2/5 p-5 mb-6">
+            <img 
+              className="block" 
+              src={candle3} 
+              alt="candles" 
+            />
+          </div>
+          <div className="border border-black w-2/5 p-5 mb-6">
+            <img 
+              className="block" 
+              src={candle4} 
+              alt="candles" 
+            />
+          </div>
+        </div>
+  );
+}
