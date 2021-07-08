321e8a32f2b3ef0cf16450a04844c16dd6e445b4
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-08 12:22:05 +0000

    styling desktop

--- src/components/MainSite.js
+++ src/components/MainSite.js
@@ -40,8 +40,8 @@ export default function MainSite() {
             </div>
           ))}  
         </div>
-        <div className="3rdrow box-border mb-10 max-w-screen-sm lg:flex lg:mb-20">
-          <div className="lg:mr-3 lg:order-last">
+        <div className="3rdrow box-border mb-10 lg:flex lg:flex-row-reverse lg:mb-20">
+          <div className="lg:mr-3 max-w-screen-sm">
             <img 
               className="block" 
               src={main2} 
