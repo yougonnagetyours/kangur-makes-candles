bba64c859f092c2e5bfe4b8dfa74f423c602a685
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-09 13:27:05 +0000

    create object instead of array with objects

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,8 @@ const candles = [
   { src: candle1, title:"Let's stay home", price:"49,90 zł", description:"49,90 zł" },
 ]
 
+const product = { src: candle1, title:"Let's stay home", price:"49,90 zł", description:"49,90 zł" } 
+
 export default function Product() {
   return (
       <div>  
@@ -12,14 +14,14 @@ export default function Product() {
             <div className="border border-black w-10/12 p-12 mb-6">
               <img 
                 className="block w-4/6 mx-auto"
-                src={candles.src} 
+                src={product.src} 
                 alt="candles" 
               />
               <div className="mt-6 mb-2">
-                <p className="text-center font-medium tracking-wider">{candles.title}</p>
+                <p className="text-center font-medium tracking-wider">{product.title}</p>
               </div>
               <div className="mt-2 mb-4">
-                <p className="text-center tracking-wider">{candles.price}</p>
+                <p className="text-center tracking-wider">{product.price}</p>
               </div>
               <div className="mx-auto my-2 py-2 border-2 border-black cursor-pointer w-auto">
                 <p className="text-center tracking-wider">Do koszyka</p>
