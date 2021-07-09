826f1c9ed26c97cec42f5deb39dfda4a55d92a73
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-09 09:57:48 +0000

    shop styling

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -16,7 +16,7 @@ export default function Shop() {
       <div>  
         <div className="flex flex-wrap justify-around box-border mx-2.5 my-6 ">
           {candles.map((candle) => (
-            <div className="border border-black w-10/12 p-12 mb-6">
+            <div className={(candle.src == candle1) ? "border border-black w-10/12 p-12 mb-6" : "border border-black w-10/12 p-24 mb-6"}>
               <img 
                 className="block" 
                 src={candle.src} 
@@ -28,10 +28,10 @@ export default function Shop() {
               <div className="mt-2 mb-2">
                 <p className="text-center tracking-wider">{candle.price}</p>
               </div>
-              <div className="mx-auto my-2 py-2 border-2 border-black cursor-pointer w-1/2">
+              <div className="mx-auto my-2 py-2 border-2 border-black cursor-pointer w-auto">
                 <p className="text-center tracking-wider">Do koszyka</p>
               </div>
-              <div className="mx-auto my-2 py-2 border-2 border-black cursor-pointer w-1/2">
+              <div className="mx-auto my-2 py-2 border-2 border-black cursor-pointer w-auto">
                 <p className="text-center tracking-wider">Kup teraz</p>
               </div>   
             </div>
