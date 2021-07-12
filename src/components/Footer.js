7175886f7d4021fad624f070a52cfa545cfe1b4e
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-12 13:42:58 +0000

    add icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import insta from '../pics/instagram.png'
+import fb from '../pics/facebook.png'
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,7 +13,16 @@ export default function Footer() {
   return (
       <div>
         <div className="my-3 text-center">
-          <p></p>
+          <img 
+              className="block" 
+              src={insta} 
+              alt="candles" 
+            />
+          <img 
+              className="block" 
+              src={fb} 
+              alt="candles" 
+            />  
         </div>  
         <div className="my-3 text-center">
           <p className="text-sm font-light tracking-widest">@2021, soyaCandles</p>
