cfe087544ac9a0f64c23b2bd16cd57bcbc9d4c6d
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-15 13:25:16 +0000

    delet backgrounds un a's

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,14 +13,14 @@ export default function Footer() {
   return (
       <div>
         <div className="my-3 w-full h-6 flex justify-center">
-          <a href="https://www.instagram.com/?hl=en" className="block mr-6 bg-gray-400">
+          <a href="https://www.instagram.com/?hl=en" className="block mr-6">
             <img 
                 className="block h-full" 
                 src={insta} 
                 alt="candles" 
               />
           </a>
-          <a href="https://www.facebook.com/" className="block bg-gray-500">
+          <a href="https://www.facebook.com/" className="block">
             <img 
                 className="block h-full" 
                 src={fb} 
