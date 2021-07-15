5001b280d33d548d19ae67b1da40863edafa0673
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-15 12:56:41 +0000

    add hyperlinks to social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,16 +13,20 @@ export default function Footer() {
   return (
       <div>
         <div className="my-3 w-full h-6 flex justify-center">
-          <img 
-              className="block mr-8" 
-              src={insta} 
-              alt="candles" 
-            />
-          <img 
-              className="block" 
-              src={fb} 
-              alt="candles" 
-            />  
+          <a href="https://www.instagram.com/?hl=en" className="mr-6"
+            <img 
+                className="block" 
+                src={insta} 
+                alt="candles" 
+              />
+          </a>
+          <a href="https://www.facebook.com/"
+            <img 
+                className="block" 
+                src={fb} 
+                alt="candles" 
+              />  
+           </a> 
         </div>  
         <div className="my-3 text-center">
           <p className="text-sm font-light tracking-widest">@2021, soyaCandles</p>
