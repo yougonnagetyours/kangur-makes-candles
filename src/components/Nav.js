590e939fc126ce15d7c2e1bd66f4aa829a170b8b
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-07-16 11:33:17 +0000

    styling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,8 +23,8 @@ function classNames(...classes) {
 
 export default function Nav() {
   return (
-    <div>
-    <Disclosure as="nav" className="bg-white fixed top-0 right-0 w-full sm:static">
+    <div className="border-b">
+    <Disclosure as="nav" className="bg-white = fixed top-0 right-0 w-full h-16 sm:static sm:h-auto">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
