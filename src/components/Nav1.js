20e6103b3be354da096745425203f6bae86f5b9a
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-09 12:53:40 +0000

    styling nav

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -3,6 +3,7 @@ import { Fragment, useEffect, useRef, useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { SearchIcon, MenuIcon, XIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 import { Link } from "react-router-dom"
+import brand from '../pics/candles.png'
 
 const navigation = [
   { name: 'Sklep', path: '/shop', current: false },
@@ -23,7 +24,7 @@ export default function Nav1() {
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="inline-flex items-center justify-center p-2 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -31,7 +32,53 @@ export default function Nav1() {
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
           </Menu.Button>
-        </div>
+          </div>
+
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <Link to="/">
+                  <div className="flex-shrink-0 flex items-center">
+                    <img
+                      className="block lg:hidden h-8 w-auto"
+                      src={brand}
+                      alt="candles"
+                    />
+                    <img
+                      className="hidden lg:block h-8 w-auto"
+                      src={brand}
+                      alt="candles"
+                    />
+                    <p className="tracking-wider mx-2">soyaCandles</p>
+                  </div>
+                </Link>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link to={item.path}>
+                        <div
+                          key={item.name}
+                          className={classNames(
+                            item.current ? 'border-2 border-gray-900 text-black' : 'text-black hover:border-2 border-gray-700 hover:text-gray-700 hover:underline',
+                            'px-3 py-2 rounded-md text-sm tracking-widest'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button className="bg-white p-1 rounded-full text-black">
+                  <span className="sr-only">View notifications</span>
+                  <SearchIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+                <button className="bg-white p-1 rounded-full text-black focus:outline-none">
+                  <span className="sr-only">View notifications</span>
+                  <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
 
         </div>
         </div>
