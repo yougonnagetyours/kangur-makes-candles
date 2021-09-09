2dcfff3b1fbd6d306aaab062df8b118691f523ad
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-09 09:36:29 +0000

    fix bugs

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -10,7 +10,7 @@ export default function Nav1() {
         <div className="sm:hidden">
           <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             <span className="sr-only">Open main menu</span>
-                  {open ? (
+                  {active ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
