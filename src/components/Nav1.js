afc1a9cece8d1dbaf6075b8682a7778c21fc4789
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-10 07:11:33 +0000

    bug fix

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -97,7 +97,7 @@ export default function Nav1() {
         >
         
           <Menu.Items className="absolute left-0 w-full origin-top-right bg-white divide-y divide-gray-100 text-center focus:outline-none">
-            <div className={open ? "border-b", "px-1 py-12 border-b"}>
+            <div className={open ? "border-b" : "", "px-1 py-12 border-b"}>
             {({open}) => (
               {navigation.map((item) =>(
                 <Menu.Item>
