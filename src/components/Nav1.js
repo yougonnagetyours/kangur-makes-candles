315f97b7c95c5e0d45d5982e87dff217786cafef
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-10 07:29:08 +0000

    border-b another tryout

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -24,7 +24,7 @@ export default function Nav1() {
       <Menu as="div" className="bg-white fixed top-0 right-0 w-full sm:static">
         {({ open }) => (
         <>  
-        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className={open ? "" : "border-b", "max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"}>
         <div className="relative flex items-center justify-between h-16">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <Menu.Button className="inline-flex items-center justify-center p-2 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -96,8 +96,8 @@ export default function Nav1() {
           leaveTo="transform opacity-0 scale-95"
         >
         
-          <Menu.Items className="absolute left-0 w-full origin-top-right py-16 bg-white divide-y divide-gray-100 border-b text-center focus:outline-none">
-            <div className="px-1 py-1 ">
+          <Menu.Items className="absolute left-0 w-full origin-top-right bg-white divide-y divide-gray-100 text-center focus:outline-none">
+            <div className="px-1 py-12 border-b">
               {navigation.map((item) =>(
                 <Menu.Item>
                   <Link to={item.path}>
@@ -105,7 +105,7 @@ export default function Nav1() {
                       key={item.name}
                       className={classNames(
                         item.current ? 'underline text-black' : 'text-black',
-                        'block px-5 py-2 rounded-md text-base tracking-widest'
+                        'block px-2 py-5 rounded-md text-base tracking-widest'
                       )}
                       aria-current={item.current ? 'page' : undefined}
                     >
