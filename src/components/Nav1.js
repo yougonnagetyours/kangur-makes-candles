ce5a7db2517945787276449a62f75b411b024734
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-09-10 07:09:07 +0000

    add if for border-b in nav1

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -24,7 +24,7 @@ export default function Nav1() {
       <Menu as="div" className="bg-white fixed top-0 right-0 w-full sm:static">
         {({ open }) => (
         <>  
-        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 border-b">
+        <div className={open ? "" : "border-b", "max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"}>
         <div className="relative flex items-center justify-between h-16">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <Menu.Button className="inline-flex items-center justify-center p-2 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -96,8 +96,9 @@ export default function Nav1() {
           leaveTo="transform opacity-0 scale-95"
         >
         
-          <Menu.Items className="absolute left-0 w-full origin-top-right py-12 bg-white divide-y divide-gray-100 text-center focus:outline-none">
-            <div className="px-1 py-1 border-b">
+          <Menu.Items className="absolute left-0 w-full origin-top-right bg-white divide-y divide-gray-100 text-center focus:outline-none">
+            <div className={open ? "border-b", "px-1 py-12 border-b"}>
+            {({open}) => (
               {navigation.map((item) =>(
                 <Menu.Item>
                   <Link to={item.path}>
@@ -131,7 +132,8 @@ export default function Nav1() {
                     />  
                   </a> 
                 </div>  
-              </div>    
+              </div> 
+            )}
             </div>
           </Menu.Items>
         </Transition>
