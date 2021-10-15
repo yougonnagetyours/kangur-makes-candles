3b82065943d445d1b9ebf3f700eb4a9ea204039e
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-10-15 07:11:15 +0000

    Add photo to shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,6 +4,7 @@ import candle1 from '../pics/candle1.jpg'
 import candle2 from '../pics/candle2.jpg'
 import candle3 from '../pics/candle3.jpg'
 import candle4 from '../pics/candle4.jpg'
+import main2 from '../pics/main2.jpg'
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,7 +20,21 @@ const candles = [
 ]
 
 export default function Shop() {
-  return ( 
+  return (
+    <>
+        <div className="3rdrow box-border mb-10 lg:flex lg:flex-row-reverse lg:mb-20">
+          <div className="lg:mr-3 max-w-screen-lg">
+            <img 
+              className="block" 
+              src={main2} 
+              alt="candles" 
+            />
+          </div>
+          <div className="mt-6">
+            <p className="text-2xl sm:text-4xl mx-6 mt-3 tracking-widest">Naturalne składniki</p>
+            <p className="text-sm font-light sm:text-xl mx-6 mt-3 tracking-widest">To coś co wyróżnia soyaCandles</p>
+          </div>
+        </div> 
         <div className="flex flex-wrap justify-around box-border mx-2.5 my-6 ">
           {candles.map((candle) => (
             <div className="w-10/12 mb-20">
@@ -47,5 +62,6 @@ export default function Shop() {
             </div>
           ))}  
         </div>  
+    </>    
   );
 }
