82ddd06c21fa8cce0d40ded6e12e1725e24b5c97
yougonnagetyours <michal.potoczny.92@gmail.com>
2021-11-08 18:23:34 +0100

    implement cart to routing in app.js and pass props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav1 from './components/Navbar/Nav1.js'
 import MainSite from './components/MainSite/MainSite.js'
 import Shop from './components/Shop/Shop.js'
 import ProductDetails from './components/ProductDetails/ProductDetails'
+import Cart from './components/Cart/Cart'
 import Footer from './components/Footer/Footer.js'
 import ScrollToTop from './components/scrollToTop.js'
 
@@ -13,8 +14,6 @@ import {
   Route,
   Link
 } from "react-router-dom"
-//import ShopPrewiew from './components/ShopPreview.js'
-
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -28,8 +27,43 @@ function App() {
     setProducts(data);
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve()); 
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const { cart } = await commerce.cart.add(productId, quantity);
+
+    setCart(cart);
+  };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+      const { cart } = await commerce.cart.update(productId, { quantity });
+
+      setCart(cart);
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+      const { cart } = await commerce.cart.remove(productId);
+
+      setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+      const { cart } = await commerce.cart.empty();
+      
+      setCart(cart);
+  }
+
+  const refreshCart = async () => {
+      const newCart = await commerce.cart.refresh();
+
+      setCart(newCart);
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
   console.log(products);
@@ -46,6 +80,14 @@ function App() {
               <Shop products={products} />
             </Route>
             <Route exact path="/:id" children={<ProductDetails products={products} onAddToCart={""} />}></Route>
+            <Route path="/cart">
+              <Cart 
+                cart={cart}
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleEmptyCart={handleEmptyCart} 
+              />
+            </Route>
           </Switch>
         </Fragment>
       </main>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Link, useLocation } from 'react-router-dom';
+
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
