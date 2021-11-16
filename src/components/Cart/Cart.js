5bc67033b291db094bfd6bb674d86ca6657dbcff
yougonnagetyours <66637616+yougonnagetyours@users.noreply.github.com>
2021-11-16 10:50:09 +0100

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -37,7 +37,7 @@ const FilledCart = () => (
                 <p className="text-center text-base tracking-widest">Wyczyść koszyk</p>
               </div>
               <Link to="/checkout" className="border-2 border-black cursor-pointer px-4 py-2">                
-                <p className="text-center tracking-widest">Do kasy</p>
+                <button className="text-center tracking-widest">Do kasy</button>
               </Link>
           </div>
       </div>
