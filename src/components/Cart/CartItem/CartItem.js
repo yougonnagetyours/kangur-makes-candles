57ca4ae66b5905485331853450e2d8e1f431896e
yougonnagetyours <66637616+yougonnagetyours@users.noreply.github.com>
2021-11-16 11:25:29 +0100

    Styling CartItem.js

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -16,12 +16,12 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
       <div className="my-2">
         <p className="text-center text-base tracking-widest">{`${item.price.formatted * item.quantity} zł`}</p>
       </div>
-      <div className="flex justify-center mt-6">
+      <div className="flex justify-center mt-2">
         <button className="mx-2 cursor-pointer" onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</button>
         <p className="mx-2">{item.quantity}</p>
         <button className="mx-2 cursor-pointer" onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</button>
       </div>
-      <div className="text-center text-base tracking-widest border-2 border-black mt-6 p-2 cursor-pointer" onClick={() => onRemoveFromCart(item.id)}>Usuń z koszyka</div>
+      <div className="text-center text-base tracking-widest border-2 border-black mt-2 p-2 cursor-pointer" onClick={() => onRemoveFromCart(item.id)}>Usuń z koszyka</div>
     </div>
   )
 }
