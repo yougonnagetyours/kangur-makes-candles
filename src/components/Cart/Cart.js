f15ca8b012dcdc6a729d18de3767d1406449ccbf
yougonnagetyours <66637616+yougonnagetyours@users.noreply.github.com>
2021-11-16 11:00:02 +0100

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -32,10 +32,13 @@ const FilledCart = () => (
           <div className="text-base tracking-widest font-medium">
               Razem: {cart.subtotal.formatted} zł
           </div>
-          <div className="flex justify-between w-10/12 mx-auto my-6">
-              <div className="cursor-pointer my-2 border-b-2 border-black" onClick={handleEmptyCart}>
+          <div className="cursor-pointer my-2" onClick={handleEmptyCart}>
                 <p className="text-center text-base tracking-widest">Wyczyść koszyk</p>
-              </div>
+          </div>
+          <div className="flex justify-between w-10/12 mx-auto my-6">
+              <Link to="/shop" className="cursor-pointer my-2 border-b-2 border-black" onClick={handleEmptyCart}>
+                <p className="text-center text-base tracking-widest">Powrót do sklepu</p>
+              </Link>
               <Link to="/checkout" className="border-2 border-black cursor-pointer px-4 py-2">                
                 <p className="text-center text-base tracking-widest">Do kasy</p>
               </Link>
