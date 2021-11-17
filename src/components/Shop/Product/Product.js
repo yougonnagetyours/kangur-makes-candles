73558a93f3c71debcbe7b8c6ffd0de853e54cec2
yougonnagetyours <66637616+yougonnagetyours@users.noreply.github.com>
2021-11-17 11:59:03 +0100

    Update Product.js

--- src/components/Shop/Product/Product.js
+++ src/components/Shop/Product/Product.js
@@ -6,7 +6,7 @@ const Product = ({ product, onAddToCart }) => {
   const handleAddToCart = () => onAddToCart(product.id, 1);
 
   return (
-      <div className="">      
+      <div className="sm:w-1/3 sm:px-2">      
         <Link to={`/${product.id}`}>
         <div className="border border-black">
           <img 
