e373659d19af4fb0ace157af661604bed639b929
yougonnagetyours <66637616+yougonnagetyours@users.noreply.github.com>
2021-11-17 08:54:54 +0100

    Update Review.js

--- src/components/CheckoutForm/Review.js
+++ src/components/CheckoutForm/Review.js
@@ -10,20 +10,20 @@ const Review = ({ checkoutToken }) => {
                 {checkoutToken.live.line_items.map((product) => (
                     <div className="w-6/12 mx-auto mt-10 mb-6" key={product.id}>
                         <div className="my-2">
-                            <p className="text-center text-base tracking-widest">{`ilość: ${product.name}`}</p>
+                            <p className="text-base tracking-widest">{`ilość: ${product.name}`}</p>
                         </div>
                         <div className="my-2">
-                            <p className="text-center text-base tracking-widest">{`ilość: ${product.quantity}`}</p>
+                            <p className="text-base tracking-widest">{`ilość: ${product.quantity}`}</p>
                         </div>
                         <div className="my-2">
-                            <p className="text-center text-lg tracking-widest">{`${product.line_total.formatted} zł`}</p>
+                            <p className="text-lg tracking-widest">{`${product.line_total.formatted} zł`}</p>
                         </div>
                     </div>
                 ))}
                 <div className="w-6/12 mx-auto mt-10 mb-6">
                     <p className="text-center text-lg tracking-widest">Suma</p>
                     <div className="text-center">
-                        <p className="font-medium text-lg tracking-widest">{`${checkoutToken.live.subtotal.formatted} zł`}</p>
+                        <p className="font-medium text-lg tracking-widest border-b-2 border-black">{`${checkoutToken.live.subtotal.formatted} zł`}</p>
                     </div>
                 </div>
             </div>
