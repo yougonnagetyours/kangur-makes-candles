140ea5e4181bc0cfeca686246ffd10f6b12c2970
yougonnagetyours <66637616+yougonnagetyours@users.noreply.github.com>
2021-11-17 12:35:26 +0100

    Update Shop.js

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -24,7 +24,7 @@ const Shop = ({ products, onAddToCart }) => {
             <p className="text-sm font-light sm:text-xl mx-6 mt-3 tracking-widest">Poszukaj dla siebie coś odpowiedniego</p>
           </div>
         </div> 
-        <div className="flex flex-wrap justify-around box-border mx-2.5 my-6 ">
+        <div className="my-6 ">
           {products.map((product) => (
             <div className="w-10/12 mb-20 sm:flex sm:-mx-2" item key={product.id}>            
                 <Product product={product} onAddToCart={onAddToCart} />           
