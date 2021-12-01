7804b026ad5eb142f605d4d601cfc16f60b81cc3
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-12-01 13:31:40 +0000

    change number link for dummy

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ return (
       <p className="text-xl sm:text-2xl md:text-3xl tracking-widest">Kontakt</p>
       <div className="mt-6">
         <p className="text-sm font-light uppercase text-justify md:text-base mx tracking-widest">Telefon:</p>
-        <a className="block text-sm font-light border-b w-max border-black md:text-base mx tracking-widest" href="tel:+48694773906">+48 111 111 111</a>
+        <a className="block text-sm font-light border-b w-max border-black md:text-base mx tracking-widest" href="tel:+48111111111">+48 111 111 111</a>
       </div>
       <div className="mt-6">
         <p className="text-sm font-light uppercase text-justify md:text-base mx tracking-widest">Napisz do nas:</p>
