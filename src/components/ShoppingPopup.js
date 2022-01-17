7d231ed2c7f2950e6f790606a83ad634841cd6c1
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-01-17 15:56:57 +0100

    popup styling

--- src/components/ShoppingPopup.js
+++ src/components/ShoppingPopup.js
@@ -3,7 +3,7 @@ import React from "react";
 const ShoppingPopup = () => {
   return (
     <div className='fixed left-0 bottom-0 flex justify-center w-full'>
-       <div className='bg-black mx-2 mb-2 w-full text-center tracking-widest font-light text-white p-4'>
+       <div className='bg-black mx-2 sm:mx-10 md:mx-0 mb-2 w-full md:w-max text-center tracking-widest font-light text-white p-4'>
           Produkt został dodany do koszyka !
        </div>
     </div>
