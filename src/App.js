d08eac3fa048366b9f88786a61a06b8340ab7def
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-01-17 14:56:29 +0100

    final popup styling

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ function App() {
           <Footer />
           {isAddedToCart ? 
            <div className='fixed left-0 bottom-0 flex justify-center w-full'>
-             <div className='bg-black mx-2 mb-2 text-center tracking-widest font-light text-white p-4'>
+             <div className='bg-black mx-2 mb-2 w-full text-center tracking-widest font-light text-white p-4'>
                 Produkt został dodany do koszyka !
              </div>
            </div>
