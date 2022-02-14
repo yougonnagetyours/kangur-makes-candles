45c3c961c5a0cfff2b7688518314597934a7ce55
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-02-14 10:21:32 +0000

    styling searchView

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,7 +5,7 @@ const SearchResults = ({ products, search, onAddToCart, clearInput }) => {
   return (
     <div className='flex flex-wrap box-border mx-2.5 my-6'>
        {search(products).map((product) => (
-         <div className="px-2 mb-20 sm:w-1/3 lg:w-1/4 sm:px-2 sm:mb-10" item key={product.id}>
+         <div className="px-20 mb-20 sm:w-1/3 lg:w-1/4 sm:px-2 sm:mb-10" item key={product.id}>
            <div onClick={clearInput}>
             <Product product={product} onAddToCart={onAddToCart} clearInput={clearInput} />
           </div>
